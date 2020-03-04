1b1df4d92231b484cd92f88d223ca4ae93258594
Kaic Melo <kaicmelosantos@gmail.com>
2020-03-04 08:25:46 -0300

    Conclusão de projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'screens/contacts_list.dart';
+import 'screens/dashboard.dart';
+import 'screens/contact_form.dart';
+import 'package:bytebankmemory/models/contact.dart';
+import 'package:bytebankmemory/database/app_database.dart';
 
 void main() => runApp(BytebanckApp());
 
@@ -7,10 +12,16 @@ class BytebanckApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Dashboard'),
-        ),
+      theme: ThemeData(
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          ),
       ),
+      home: Dashboard(),
     );
   }
 }
+
---
+++ lib/models/contact.dart
@@ -0,0 +1,12 @@
+class Contact {
+  final String name;
+  final int accountNumber;
+  final int id;
+
+  Contact(this.id, this.name, this.accountNumber);
+
+  @override
+  String toString() {
+    return 'Contact{id: $id,name: $name, accountNumber: $accountNumber}';
+  }
+}
